572da9fd2026861607b064960d688e52eac6d854
unknown <binyaalex@gmail.com>
2022-10-11 15:22:54 +0300

    start soroka

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,7 +1,52 @@
 const Main = () => {
+
+    
+	const MainArr = [
+		{
+			img: '',
+            color: 'blue',
+			text: 'נקיון כניסה',
+			link: '#',
+		},
+		{
+			img: '',
+            color: 'blue',
+			text: 'תחזוקת מעליות',
+			link: '#',
+		},
+		{
+			img: '',
+            color: 'blue',
+			text: 'פינוי זבל',
+			link: '#',
+		},
+		{
+			img: '',
+            color: 'blue',
+			text: 'אבטחה',
+			link: '#',
+		},
+		{
+			img: '',
+            color: 'blue',
+			text: 'שיפוץ',
+			link: '#',
+		},
+    ]
+
     return (
         <>
-            S
+            <main className='mainPage' >
+                {
+                    MainArr.map((el, i) => {
+                        return (
+                            <div>
+                                {el.text}
+                            </div>
+                        )
+                    })
+                }
+            </main>
         </>
     )
 }
