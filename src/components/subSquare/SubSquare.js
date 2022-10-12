7194e68c217995715346881d2c131b391949aa42
ziv-hagai <zivhag@gmail.com>
2022-10-12 14:40:06 +0300

    css fix3

--- src/components/subSquare/SubSquare.js
+++ src/components/subSquare/SubSquare.js
@@ -4,13 +4,13 @@ import Details from "../details/Details";
 const SubSquare = (props) => {
     const [show, setShow] = useState(false);
     const handleShow = () => setShow(true);
-    
+
     return (
         <div className="category" onClick={handleShow} >
-            <div className="imgDiv" style={{ backgroundColor: `var(--${props.color})` }}>
-                <img src={props.el.img} />
+            <img src={props.el.img} />
+            <div className='textDiv'>
+                <h2 style={{ color: `var(--${props.color})` }}>{props.el.title}</h2>
             </div>
-            <h2 style={{ color: `var(--${props.color})` }}>{props.el.title}</h2>
             <Details show={show} setShow={setShow} object={props.el} />
         </div>
     )
